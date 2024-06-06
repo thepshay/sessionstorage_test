6225cf6ac3966c82dc9e4d64c15d74974af4e254
thepshay <perry.xie1@gmail.com>
2024-06-06 11:38:19 -0700

    update params

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 window.addEventListener('load', () => {
-  if (sessionStorage.getItem('tagged')) {
+  if (sessionStorage.getItem('tgb_app')) {
+    console.log(sessionStorage.getItem('tgb_app'))
+    console.log(`email ${sessionStorage.getItem('user_email')}`)
     document.body.style.backgroundColor = 'red';
   }
 });
